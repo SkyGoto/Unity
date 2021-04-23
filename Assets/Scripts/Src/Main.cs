74a4bc339afe1611b469882c420dfb28e5f0f095
zhouzhe <z_zmail@126.com>
2021-04-23 09:51:25 +0800

    添加一个加载器

--- Assets/Scripts/Src/Main.cs
+++ Assets/Scripts/Src/Main.cs
@@ -1,33 +1,26 @@
-﻿/*
- * Tencent is pleased to support the open source community by making xLua available.
- * Copyright (C) 2016 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
- * http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
-
+﻿using System;
 using System.IO;
 using UnityEngine;
 using XLua;
 
-namespace XLuaTest
+namespace MainLuaCode
 {
-    public class Helloworld : MonoBehaviour
+    public class Main : MonoBehaviour
     {
         // Use this for initialization
-        void Start()
+        private void Start()
         {
             LuaEnv luaenv = new LuaEnv();
             luaenv.AddLoader(MyLoader);
-            luaenv.DoString(@"require 'hello_world'");
+            luaenv.DoString(@"require 'main'");  // 可以通过自定义加载器实现，也可以通过package.path来定义
             luaenv.Dispose();
         }
-
         // Update is called once per frame
         void Update()
         {
 
         }
+        // Custom file loader
         private byte[] MyLoader(ref string filePath)
         {
 #if UNITY_EDITOR
